dc5b76799bf29b09f43da7d8c138e56be907e6a9
shahzad11khan <61977658+shahzad11khan@users.noreply.github.com>
2024-09-03 16:09:27 +0500

    changes

--- src/app/api/Blog/[blogID]/route.js
+++ src/app/api/Blog/[blogID]/route.js
@@ -1,16 +1,155 @@
+// const { connect } = require("@/app/config/db");
+// const { default: BlogModel } = require("@/app/models/BlogModel");
+// const { NextResponse } = require("next/server");
+// import { unlink, writeFile } from "fs/promises";
+// import path from "path";
+
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
+
+// // Delete team member
+// export async function DELETE(request, context) {
+//   try {
+//     const id = context.params.blogID;
+//     console.log(id);
+
+//     // Connect to the database
+//     await connect();
+
+//     // Find the user by ID
+//     const Find_Project = await BlogModel.findById(id);
+
+//     // Check if the user exists
+//     if (!Find_Project) {
+//       return NextResponse.json({ message: "Request not found", status: 404 });
+//     }
+
+//     // Get the image file path
+//     // const filePath = `./public/uploads/${file.name}`;
+//     const imagePath = path.join("./public/uploads/", Find_Project.image);
+//     console.log(imagePath);
+//     // return;
+
+//     // Delete the user from the database
+//     const _deletedblog = await BlogModel.findByIdAndDelete(id);
+//     console.log(_deletedblog);
+//     // Check if the user was found and deleted
+//     if (!_deletedblog) {
+//       return NextResponse.json({
+//         message: "Project not found",
+//         status: 404,
+//       });
+//     }
+
+//     // Delete the image file from the filesystem
+//     try {
+//       await unlink(imagePath);
+//       console.log(`Deleted file: ${imagePath}`);
+//     } catch (error) {
+//       console.error(`Failed to delete file: ${imagePath}`, error);
+//     }
+
+//     return NextResponse.json({
+//       message: "Project deleted successfully",
+//       status: 200,
+//     });
+//   } catch (error) {
+//     console.error("Error deleting user:", error);
+//     // Return an error response
+//     return NextResponse.json({ error: "Failed to delete user", status: 500 });
+//   }
+// }
+
+// // get Secific
+// export async function GET(request, context) {
+//   try {
+//     await connect();
+//     const id = context.params.blogID;
+//     console.log(id);
+//     const Find_project = await BlogModel.findById(id);
+//     if (!Find_project) {
+//       return NextResponse.json({ result: "No Request Availible" });
+//     } else {
+//       return NextResponse.json({ Result: Find_project });
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json({ Message: "Internal Server Error " });
+//   }
+// }
+
+// // pages/api/users/[userID].js
+// export async function PUT(request, context) {
+//   try {
+//     await connect();
+//     const id = context.params.blogID;
+//     console.log(id);
+//     const data = await request.formData();
+
+//     const file = data.get("image");
+//     let filename = null;
+//     let buffer = null;
+
+//     if (typeof file === "object") {
+//       filename = file.name;
+//       const byteData = await file.arrayBuffer();
+//       buffer = Buffer.from(byteData);
+//       const filePath = `./public/uploads/${filename}`;
+//       await writeFile(filePath, buffer);
+//     }
+
+//     const formDataObject = {};
+
+//     // Iterate over form data entries
+//     for (const [key, value] of data.entries()) {
+//       // Assign each field to the formDataObject
+//       formDataObject[key] = value;
+//     }
+
+//     const { blogtitle, author, datetime, description } = formDataObject;
+//     console.log(blogtitle, author, datetime, description);
+
+//     // Check if the user exists
+//     const blog = await BlogModel.findById(id);
+//     if (!blog) {
+//       return NextResponse.json({ error: "User not found" }, { status: 404 });
+//     }
+
+//     // Update the user details
+//     blog.blogtitle = blogtitle || blog.blogtitle;
+//     blog.author = author || blog.author;
+//     blog.datetime = datetime || blog.datetime;
+//     blog.description = description || blog.description;
+
+//     if (filename) {
+//       blog.image = filename;
+//     }
+
+//     await blog.save();
+
+//     return NextResponse.json({
+//       message: "Record updated successfully",
+//       blog,
+//     });
+//   } catch (error) {
+//     console.error("Error Updating User:", error);
+//     return NextResponse.json(
+//       { error: "Failed to update user" },
+//       { status: 500 }
+//     );
+//   }
+// }
+
 const { connect } = require("@/app/config/db");
 const { default: BlogModel } = require("@/app/models/BlogModel");
 const { NextResponse } = require("next/server");
 import { unlink, writeFile } from "fs/promises";
 import path from "path";
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-// Delete team member
+// Delete a blog post
 export async function DELETE(request, context) {
   try {
     const id = context.params.blogID;
@@ -19,27 +158,26 @@ export async function DELETE(request, context) {
     // Connect to the database
     await connect();
 
-    // Find the user by ID
+    // Find the blog by ID
     const Find_Project = await BlogModel.findById(id);
 
-    // Check if the user exists
+    // Check if the blog exists
     if (!Find_Project) {
-      return NextResponse.json({ message: "Request not found", status: 404 });
+      return NextResponse.json({ message: "Blog not found", status: 404 });
     }
 
     // Get the image file path
-    // const filePath = `./public/uploads/${file.name}`;
     const imagePath = path.join("./public/uploads/", Find_Project.image);
     console.log(imagePath);
-    // return;
 
-    // Delete the user from the database
+    // Delete the blog from the database
     const _deletedblog = await BlogModel.findByIdAndDelete(id);
     console.log(_deletedblog);
-    // Check if the user was found and deleted
+
+    // Check if the blog was found and deleted
     if (!_deletedblog) {
       return NextResponse.json({
-        message: "Project not found",
+        message: "Blog not found",
         status: 404,
       });
     }
@@ -53,17 +191,16 @@ export async function DELETE(request, context) {
     }
 
     return NextResponse.json({
-      message: "Project deleted successfully",
+      message: "Blog deleted successfully",
       status: 200,
     });
   } catch (error) {
-    console.error("Error deleting user:", error);
-    // Return an error response
-    return NextResponse.json({ error: "Failed to delete user", status: 500 });
+    console.error("Error deleting blog:", error);
+    return NextResponse.json({ error: "Failed to delete blog", status: 500 });
   }
 }
 
-// get Secific
+// Get a specific blog post
 export async function GET(request, context) {
   try {
     await connect();
@@ -71,7 +208,7 @@ export async function GET(request, context) {
     console.log(id);
     const Find_project = await BlogModel.findById(id);
     if (!Find_project) {
-      return NextResponse.json({ result: "No Request Availible" });
+      return NextResponse.json({ result: "No Blog Available" });
     } else {
       return NextResponse.json({ Result: Find_project });
     }
@@ -81,7 +218,7 @@ export async function GET(request, context) {
   }
 }
 
-// pages/api/users/[userID].js
+// Update a blog post
 export async function PUT(request, context) {
   try {
     await connect();
@@ -105,20 +242,19 @@ export async function PUT(request, context) {
 
     // Iterate over form data entries
     for (const [key, value] of data.entries()) {
-      // Assign each field to the formDataObject
       formDataObject[key] = value;
     }
 
     const { blogtitle, author, datetime, description } = formDataObject;
     console.log(blogtitle, author, datetime, description);
 
-    // Check if the user exists
+    // Check if the blog exists
     const blog = await BlogModel.findById(id);
     if (!blog) {
-      return NextResponse.json({ error: "User not found" }, { status: 404 });
+      return NextResponse.json({ error: "Blog not found" }, { status: 404 });
     }
 
-    // Update the user details
+    // Update the blog details
     blog.blogtitle = blogtitle || blog.blogtitle;
     blog.author = author || blog.author;
     blog.datetime = datetime || blog.datetime;
@@ -131,13 +267,13 @@ export async function PUT(request, context) {
     await blog.save();
 
     return NextResponse.json({
-      message: "Record updated successfully",
+      message: "Blog updated successfully",
       blog,
     });
   } catch (error) {
-    console.error("Error Updating User:", error);
+    console.error("Error Updating Blog:", error);
     return NextResponse.json(
-      { error: "Failed to update user" },
+      { error: "Failed to update blog" },
       { status: 500 }
     );
   }
