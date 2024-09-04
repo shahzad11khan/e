7b6902051b41618c1161d984170fe01703d32328
shahzad11khan <61977658+shahzad11khan@users.noreply.github.com>
2024-09-04 11:16:46 +0500

    change url

--- src/app/AdminDashboard/components/ShowApidatas/apiUrls.js
+++ src/app/AdminDashboard/components/ShowApidatas/apiUrls.js
@@ -1,11 +1,12 @@
 // apiUrls.js
 
-export const API_URL_USER = "/api/Users";
-export const API_URL_Verify = "/api/Users/verifyEmail";
-export const API_URL_TEAM = "/api/Team";
-export const API_URL_Vacancy = "/api/Vacancies/postVacancy";
-export const API_URL_Vacancy_Delete = "/api/Vacancies";
-export const API_URL_GetInTouch = "/api/GetInTouch";
-export const API_URL_Request = "/api/RequestForJoining";
-export const API_URL_Projects = "/api/Project";
-export const API_URL_Blog = "https://e-omega-inky.vercel.app/api/Blog";
+const url = "https://e-omega-inky.vercel.app";
+export const API_URL_USER = `${url}/api/Users`;
+export const API_URL_Verify = `${url}/api/Users/verifyEmail`;
+export const API_URL_TEAM = `${url}/api/Team`;
+export const API_URL_Vacancy = `${url}/api/Vacancies/postVacancy`;
+export const API_URL_Vacancy_Delete = `${url}/api/Vacancies`;
+export const API_URL_GetInTouch = `${url}/api/GetInTouch`;
+export const API_URL_Request = `${url}/api/RequestForJoining`;
+export const API_URL_Projects = `${url}/api/Project`;
+export const API_URL_Blog = `${url}/api/Blog`;
