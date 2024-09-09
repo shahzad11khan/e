3c92bd235d731996fc251a8d48a6acddaf7362b9
shahzad11khan <61977658+shahzad11khan@users.noreply.github.com>
2024-09-09 12:49:28 +0500

    verify user

--- src/app/AdminDashboard/RegisterAdmin/page.js
+++ src/app/AdminDashboard/RegisterAdmin/page.js
@@ -78,6 +78,7 @@ const AdminTable = () => {
       if (response.status === 200) {
         console.log("Verification status updated successfully:", response.data);
         // setIsVerified(newStatus); // Update the local state to reflect the new status
+        getAdmin();
       } else {
         console.error(
           "Failed to update verification status:",
